b705a94e2f6c3937a39a3dadd15af6ae01efae5e
kliu37 <76674412+kliu37@users.noreply.github.com>
2021-02-24 09:53:07 -0800

    Create WK4 graded project.py

---
+++ WK4 graded project.py
@@ -0,0 +1,2 @@
+#function argument, no space between equal operator. eg. sorted(student_tuples, key=lambda student: student[2], reverse=True)
+#but when assign value to variable, need space between equal operator. eg: a = 3
